748a12db2606df489ad9d3ef8e0c7b7e6542b38d
Devansh Chawla <devanshchawla@Devanshs-MacBook-Air.local>
2024-09-17 07:38:59 +0530

    EP-1

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement('div', {id:'parent'}, [
+    React.createElement('div', {id:'child1'}, [
+        React.createElement('h1', {id:'1h1'}, '1h1'),
+        React.createElement('h2', {id:'1h2'}, '1h2')
+    ]),
+    React.createElement('div', {id:'child2'}, [
+        React.createElement('h1', {id:'2h1'}, '2h1'),
+        React.createElement('h2', {id:'2h2'}, '2h2')
+    ])
+])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent);
