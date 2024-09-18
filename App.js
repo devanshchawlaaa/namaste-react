ffeab12cca38f9ea86bb53d594018a855a9f1a6b
Devansh Chawla <devanshchawla@Devanshs-MacBook-Air.local>
2024-09-18 06:48:59 +0530

    EP-2 Practice

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const parent = React.createElement('div', {id:'parent'}, [
     React.createElement('div', {id:'child1'}, [
-        React.createElement('h1', {id:'1h1'}, '1h1'),
+        React.createElement('h1', {id:'1h1'}, '1h1 dev'),
         React.createElement('h2', {id:'1h2'}, '1h2')
     ]),
     React.createElement('div', {id:'child2'}, [
